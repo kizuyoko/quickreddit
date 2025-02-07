d0f8699f97eeeb129e3d6df866fd8ac66f8f7c08
kizuyoko <kizuyoko@gmail.com>
2025-02-07 15:16:55 +0100

    Remove unnessesary button

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -49,7 +49,7 @@ export const Posts: React.FC<PostsProps> = ({ query }) => {
         <SocialMedia />
       </div>
       <div className="grid grid-cols-2 gap-4 p-4 pt-2 w-full mx-auto md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 max-w-[1800] max-[420px]:grid-cols-1">
-        {content} <button>fdfsfsfsd</button>
+        {content}
       </div>
     </>  
   );
